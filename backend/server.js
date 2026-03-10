45d6b6dcee27c89e160bcb93b02a69ca5780294e
Tri <Tringuyendinh230@gmail.com>
2026-03-10 20:46:42 +0700

    commit lấy user có id đầu tiên trong bảng và api create user (post)

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,18 @@ app.get("/users__getAll", async (req, res) => {
   res.json(result.rows);
 })
 
+app.get("/users__getById/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await pool.query("SELECT * from users where id = $1", [id]);
+  res.json(result.rows);
+});
+
+app.post("/users__create", async (req, res) => {
+  const { id, name } = req.body;
+  const result = await pool.query("INSERT INTO users (id, name) VALUES ($1, $2) RETURNING *", [id, name]);
+  res.json(result.rows[0]);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
