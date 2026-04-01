0619f7761f6258906b28bd17c13b03a358b4de95
ThienTran <thientranchi052@gmail.com>
2026-04-01 22:10:44 +0700

    mở service để frontend gọi

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const pool = require("./db");
 
@@ -9,6 +10,7 @@ const categoryRoutes = require("./routes/categoryRoutes");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", userRoutes);
 app.use("/categories", categoryRoutes);
