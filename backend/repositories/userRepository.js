bd751129e43184c975cb9b762d2c28aa893446d9
ThienTran <thientranchi052@gmail.com>
2026-04-01 19:12:58 +0700

    thêm Controller, Repository, Routes và service của user

---
+++ backend/repositories/userRepository.js
@@ -0,0 +1,33 @@
+const { user: User } = require("../models");
+
+const findAll = async () => {
+    return await User.findAll();
+};
+
+const create = async ( name, dateofbirth, email, address, isadmin ) => {
+    return await User.create({ name, dateofbirth, email, address, isadmin });
+};
+
+const update = async (id, name, dateofbirth, email, address, isadmin) => {
+    const user = await User.findByPk(id);
+    if (!user) return null;
+
+    user.name = name;
+    user.dateofbirth = dateofbirth;
+    user.email = email;
+    user.address = address;
+    user.isadmin = isadmin;
+
+    await user.save();
+    return user;
+};
+
+const remove = async (id) => {
+    const user = await User.findByPk(id);
+    if (!user) return null;
+
+    await user.destroy();
+    return user;
+}
+
+module.exports = { findAll, create, update, remove };
