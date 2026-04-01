964e08db7e690a5aa4efd8bce49054410d0d4594
Tri <Tringuyendinh230@gmail.com>
2026-04-01 21:39:32 +0700

    đăng ký 1 số đường dẫn routes mới

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,20 @@ const PORT = process.env.PORT || 3000;
 
 const userRoutes = require("./routes/userRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
+const imageRoutes = require("./routes/imageRoutes");
+const productRoutes = require("./routes/productRoutes");
+const invoiceRoutes = require("./routes/invoiceRoutes");
+const invoiceDetailRoutes = require("./routes/invoiceDetailRoutes");
 
 const app = express();
 app.use(express.json());
 
 app.use("/users", userRoutes);
 app.use("/categories", categoryRoutes);
+app.use("/images", imageRoutes);
+app.use("/products", productRoutes);
+app.use("/invoices", invoiceRoutes);
+app.use("/invoiceDetails", invoiceDetailRoutes);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
