6bbdc950c2f218570715b7462b47f12ed632336a
ThienTran <thientranchi052@gmail.com>
2026-04-01 21:27:45 +0700

    Sửa lỗi deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+
+const pool = require("./db");
+
+pool.connect();
+
 const userRoutes = require("./routes/userRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 
@@ -7,6 +12,11 @@ app.use(express.json());
 
 app.use("/users", userRoutes);
 app.use("/categories", categoryRoutes);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
