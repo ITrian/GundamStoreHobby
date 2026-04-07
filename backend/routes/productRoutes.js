28e40b3585c045bac46b6b0168595cd55da50c44
Tri <Tringuyendinh230@gmail.com>
2026-04-07 19:13:01 +0700

    thêm authen cho user, product, phone và invoice

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
+const authenticate = require("../middlewares/authen");
 
-router.get("/", productController.getAllProducts);
-router.post("/", productController.insertProduct);
-router.get("/:id", productController.getProductById);
-router.patch("/", productController.updateProduct);
-router.delete("/:id", productController.deleteProduct);
+router.get("/", authenticate.authenticateToken, productController.getAllProducts);
+router.post("/", authenticate.authenticateToken, productController.insertProduct);
+router.get("/:id", authenticate.authenticateToken, productController.getProductById);
+router.patch("/", authenticate.authenticateToken, productController.updateProduct);
+router.delete("/:id", authenticate.authenticateToken, productController.deleteProduct);
 
 module.exports = router;
