229fab026556ca0766194b5c20b148898e7d3c20
ThienTran <thientranchi052@gmail.com>
2026-04-07 19:14:20 +0700

    thêm authenToken category, image, invoiceDetail

--- backend/routes/imageRoutes.js
+++ backend/routes/imageRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const imageController = require("../controllers/imageController");
+const authenticate = require("../middlewares/authen");
 
-router.get("/", imageController.getAllImages);
-router.post("/", imageController.insertImage);
-router.get("/product/:productid", imageController.getImagesByProduct);
-router.patch("/", imageController.updateImage);
-router.delete("/:productid", imageController.deleteImage);
-router.delete("/:productid/:detail", imageController.deleteSingleImage);
+router.get("/", authenticate.authenticateToken, imageController.getAllImages);
+router.post("/", authenticate.authenticateToken, imageController.insertImage);
+router.get("/product/:productid", authenticate.authenticateToken, imageController.getImagesByProduct);
+router.patch("/", authenticate.authenticateToken, imageController.updateImage);
+router.delete("/:productid", authenticate.authenticateToken, imageController.deleteImage);
+router.delete("/:productid/:detail", authenticate.authenticateToken, imageController.deleteSingleImage);
 
 module.exports = router;
